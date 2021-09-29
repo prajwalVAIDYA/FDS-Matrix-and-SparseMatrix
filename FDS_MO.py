945924c991b8f4842791a9b7dc7a27decb0783c4
PRAJWAL PRASHANT VAIDYA <90036797+prajwalVAIDYA@users.noreply.github.com>
2021-09-29 19:25:16 +0530

    Create FDS_MO.py

---
+++ FDS_MO.py
@@ -0,0 +1,105 @@
+print("Matrix Size :")
+r=int(input("Enter Number of Rows : "))
+c=int(input("Enter Number of Columns  : "))
+print("\n")
+print("Matrix 1 Elements")
+m1=[]
+for i in range (r):
+  a=[]
+  for j in range (c):
+    a.append(int(input("Enter Elements : ")))
+  m1.append(a)
+for i in range(r):
+  for j in range (c):
+    print(m1[i][j],end=" ")
+  print()
+print("\n")
+print("Matrix 2 Elements")
+m2=[]
+for i in range (r):
+  a=[]
+  for j in range (c):
+    a.append(int(input("Enter Elements : ")))
+  m2.append(a)
+for i in range(r):
+  for j in range (c):
+    print(m2[i][j],end=" ")
+  print()
+print("\n")
+
+# Adding M1 and M2
+print("New Matrix showing Addition of M1 and M2\n")
+add=[[0 for i in range(c) ] for j in range(r) ]
+for i in range(len(m1)):
+   for j in range(len(m1[0])):
+       add[i][j] = m1[i][j] + m2[i][j]
+for k in add:
+   print(k)
+print("\n")
+
+# Subtracting M1 and M2
+print("New Matrix showing Subtracting of M1 and M2\n")
+sub=[[0 for i in range(c) ] for j in range(r) ]
+for i in range(len(m1)):
+   for j in range(len(m1[0])):
+       sub[i][j] = m1[i][j] - m2[i][j]
+for k in sub:
+   print(k) 
+print("\n")
+
+#Multiplying M1 and M2
+print("New Matrix showing Multiplication of M1 and M2\n")
+mul=[[0 for i in range(c) ] for j in range(r) ]
+for i in range(len(m1)):
+   for j in range(len(m2[0])):
+     for k in range(len(m2)):
+       mul[i][j] += m1[i][k] * m2[k][j]
+for k in mul:
+   print(k)
+print("\n")
+
+Matrix Size :
+Enter Number of Rows : 2
+Enter Number of Columns  : 2
+
+
+
+
+
+
+#OutPut
+
+#Matrix 1 Elements
+#Enter Elements : 3
+#Enter Elements : 2
+#Enter Elements : 3
+#Enter Elements : 2
+#3 2 
+#3 2 
+
+
+#Matrix 2 Elements
+#Enter Elements : 2
+#Enter Elements : 1
+#Enter Elements : 2
+#Enter Elements : 1
+#2 1 
+#2 1 
+
+
+#New Matrix showing Addition of M1 and M2
+
+#[5, 3]
+#[5, 3]
+
+
+#New Matrix showing Subtracting of M1 and M2
+
+#[1, 1]
+#[1, 1]
+
+
+#New Matrix showing Multiplication of M1 and M2
+
+#[10, 5]
+#[10, 5]
